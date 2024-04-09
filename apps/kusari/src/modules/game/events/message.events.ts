22658f940dc2b10f5f108a402963d75682092e91
Jurien Hamaker <whoami@jurien.dev>
2024-04-09 14:52:37 +0200

    feat(Kusari): Edit & Delete messages

--- apps/kusari/src/modules/game/events/message.events.ts
+++ apps/kusari/src/modules/game/events/message.events.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { SettingsService } from '../../settings/services/settings.services';
-import { Events } from 'discord.js';
+import { Events, Message, PartialMessage } from 'discord.js';
 import { Context, ContextOf, On } from 'necord';
+import { SettingsService } from '../../settings/services/settings.services';
 import { GameService } from '../services/game.service';
 
 @Injectable()
@@ -17,6 +17,83 @@ export class GameMessageEvents {
 	public async onMessageCreate(
 		@Context() [message]: ContextOf<Events.MessageCreate>,
 	) {
+		const word = this._getWord(message);
+
+		if (!word) {
+			return;
+		}
+
+		const settings = await this._settings.getSettings(message.guildId);
+		if (message.channelId !== settings.channelId) {
+			return;
+		}
+
+		return this._game.addWord(message.guildId, word, message, settings);
+	}
+
+	@On(Events.MessageDelete)
+	public async onMessageDelete(
+		@Context() [message]: ContextOf<Events.MessageDelete>,
+	) {
+		const didChange = await this._didChange(message);
+
+		if (!didChange) {
+			return;
+		}
+
+		message.channel.send(
+			`<@${message.author.id}> just deleted their guess 😒
+Last word was **${didChange}**!
+The next letter is **${didChange[didChange.length - 1]}**`,
+		);
+	}
+
+	@On(Events.MessageUpdate)
+	public async onMessageUpdate(
+		@Context() [message]: ContextOf<Events.MessageDelete>,
+	) {
+		const didChange = await this._didChange(message);
+
+		if (!didChange) {
+			return;
+		}
+
+		message.channel.send(
+			`<@${message.author.id}> just edited their guess 😒
+Last word was **${didChange}**!
+The next letter is **${didChange[didChange.length - 1]}**`,
+		);
+	}
+
+	private async _didChange(message: Message<boolean> | PartialMessage) {
+		const settings = await this._settings.getSettings(message.guildId);
+		if (message.channelId !== settings.channelId) {
+			return;
+		}
+
+		const word = this._getWord(message);
+		if (!word) {
+			return;
+		}
+
+		const game = await this._game.getCurrentGame(message.guildId);
+		if (!game) {
+			return;
+		}
+
+		const lastWord = await this._game.getLastWord(game);
+		if (!lastWord) {
+			return;
+		}
+
+		if (word !== lastWord.word) {
+			return;
+		}
+
+		return lastWord.word;
+	}
+
+	private _getWord(message: Message<boolean> | PartialMessage) {
 		if (message.author.bot) {
 			return;
 		}
@@ -33,12 +110,6 @@ export class GameMessageEvents {
 
 		const word = words[0].toLowerCase();
 
-		const settings = await this._settings.getSettings(message.guildId);
-
-		if (message.channelId !== settings.channelId) {
-			return;
-		}
-
-		return this._game.addWord(message.guildId, word, message, settings);
+		return word;
 	}
 }
