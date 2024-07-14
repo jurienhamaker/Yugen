7e6c3916da1b777fd245f766f8be72be9af961da
Jurien Hamaker <whoami@jurien.dev>
2024-07-14 11:05:53 +0200

    fix(kazu): Fix the tutorial text

--- apps/kazu/src/commands/tutorial.commands.ts
+++ apps/kazu/src/commands/tutorial.commands.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { EMBED_COLOR } from '../util/constants';
 import { getEmbedFooter } from '@yugen/util';
 import { Client, EmbedBuilder } from 'discord.js';
 import { Context, SlashCommand, SlashCommandContext } from 'necord';
+import { EMBED_COLOR } from '../util/constants';
 
 @Injectable()
 export class TutorialCommands {
@@ -15,21 +15,23 @@ export class TutorialCommands {
 	public async invite(@Context() [interaction]: SlashCommandContext) {
 		const footer = await getEmbedFooter(this._client);
 		const embed = new EmbedBuilder()
-			.setTitle(`Kusari Tutorial`)
+			.setTitle(`Kazu Tutorial`)
 			.setDescription(
 				`**How to Play:**
-- The first word can be any word
-- Each word afterwards has to start with the last letter of the previous word
+- The first count must be 1.
+- Each count afterwards has to be one number higher than the previous count. It can also be an equation when math is enabled.
 - That's it! Enjoy!
 
 **Saves:**
-You can earn saves by voting for Kusari! Each vote is worth 0.25 save & 0.5 on the weekends!
+You can earn saves by voting for Kazu! Each vote is worth 0.25 save & 0.5 on the weekends!
 A save can also be donated to the server, this will increase the server saves for collaborative save system.
 Donating a save will turn 1 personal save into 0.2 server saves.
 
 **Server Settings:**
 - Channel, specify a dedicated channel
-- Cooldown, specify a cooldown before users can add a word again`,
+- Cooldown, specify a cooldown before users can add a word again
+- Math, Wether Kazu will try to parse equations
+- Shame role, a role to apply to someone that breaks the chain`,
 			)
 			.setColor(EMBED_COLOR)
 			.setFooter(footer);
